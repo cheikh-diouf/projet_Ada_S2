bdc8abd8f74b0b2945309975137c4db938d8d431
elinavrln <elina.verleene@gmail.com>
2025-04-11 11:13:39 +0200

    Add files via upload
    Signed-off-by: elinavrln <elina.verleene@gmail.com>

---
+++ Main.adb
@@ -0,0 +1,21 @@
+WITH Ada.Text_IO, Ada.Integer_Text_IO, gestion_outils, gestion_date, gestion_personnel, liste_chaine_personnel, gestion_document;
+USE Ada.Text_IO, Ada.Integer_Text_IO, gestion_outils, gestion_date, gestion_personnel, liste_chaine_personnel, gestion_document;
+
+Procedure Main is
+--pointeur: t_pt_pers;
+
+begin
+
+--Initialisation_personnel(pointeur);
+--Affichage_liste (pointeur);
+--put_line("ajoutez une personne");
+--new_line;
+--Ajout_pers (pointeur);
+--put_line("Et voice la nouvelle liste");
+--new_line;
+--Affichage_liste (pointeur);
+
+
+end Main;
+
+
